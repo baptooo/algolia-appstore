52b62fce7d7f3594b6f124dd4829f38160fc65be
Baptiste Rios Campo <baptiste.rios@gmail.com>
2018-04-26 15:28:26 +0200

    feat(build): add react-snap for performance

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ import './index.css';
 import App from './App';
 import initAppStore from './api/appstore';
 
+const isServer = document.getElementById('root').hasChildNodes();
+
 const render = (content) =>
-  ReactDOM.render((
+  ReactDOM[isServer ? 'hydrate' : 'render']((
     <Fragment>
       <CssBaseline />
       <App content={content} />
